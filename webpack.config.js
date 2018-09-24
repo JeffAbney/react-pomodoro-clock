88227aac81339101241dbe88ac73f34ee1d0f615
JeffAbney <jeffmabney@gmail.com>
2018-09-24 17:30:08 -0500

    add loader for images

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,16 @@ module.exports = {
 	  {
 	    test: /\.css$/,
 	    use: ["style-loader", "css-loader"]	
-	  }
+	  },
+    {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      }
     ]
   },
   plugins: [
