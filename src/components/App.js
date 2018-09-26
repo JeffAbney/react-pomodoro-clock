25dfd9e3eb4627bf0889c8deae53dd8a7dc04c9e
JeffAbney <jeffmabney@gmail.com>
2018-09-26 10:57:41 -0500

    Add arrow functionality, add TimeRemaining component

--- src/components/App.js
+++ src/components/App.js
@@ -14,13 +14,21 @@ const Header = (props) => {
 }
 
 const Dial = (props) => {
-	const { sessionLength,
-	        breakLength, 
-	        timeRemaining, 
-	        timerIsRunning, 
-	        timeIsAlmostUp, 
-	        isSession } 
-	        = props.state;
+	const { 
+	  sessionLength,
+	  breakLength, 
+	  timeRemaining, 
+	  timerIsRunning, 
+	  timeIsAlmostUp, 
+	  isSession 
+	} = props.state;
+
+	const { 
+	  addSessionTime,
+	  addBreakTime,
+	  subtractSessionTime,
+	  subtractBreakTime
+	} = props;
 
 	return (
       <div className="dial-container">
@@ -30,34 +38,61 @@ const Dial = (props) => {
             {Math.round(sessionLength)}:00
           </div>
           <div className="dial-number" id="quarter-time">
-            {Math.trunc(sessionLength/4)}:{sessionLength%4/4*60}
+            {Math.trunc(sessionLength/4)}
+            :
+            {
+              sessionLength%4 === 0 ?
+              "00"
+              :
+              sessionLength%4/4*60
+            }
           </div>
           <div className="dial-number" id="half-time">
-            {Math.trunc(sessionLength/2)}:
+            {Math.trunc(sessionLength/2)}
+            :
               {sessionLength%2 === 0 ?
             	"00"
               :
                 "30"}
           </div>
           <div className="dial-number" id="three-quarter-time">
-            {Math.trunc(sessionLength*0.75)}:{Math.trunc(sessionLength%(4/3)*60*3/4)}
+            {Math.trunc(sessionLength*0.75)}
+            :
+            {
+              sessionLength%4 === 0 ?
+              "00"
+              :
+              Math.trunc(sessionLength%(4/3)*60*3/4)
+            }
           </div>
           <div className="dial-button-container play-button-container">
-            <img className="dial-button play-button" src={timerIsRunning ? pause : play} />
+            <img
+              className="dial-button play-button" 
+              src={timerIsRunning ? pause : play} 
+            />
           </div>
         </div>
         <div className="time-control-container">
-          <TimeAdjuster time={sessionLength} />
+          <TimeAdjuster id="session-adjuster"
+            time={sessionLength}
+            addTime={addSessionTime}
+            subtractTime={subtractSessionTime}
+          />
           <p className="reset-button">
           Reset
           </p>
-          <TimeAdjuster time={breakLength}/>
+          <TimeAdjuster id="break-adjuster"
+            time={breakLength}
+            addTime={addBreakTime}
+            subtractTime={subtractBreakTime}
+          />
         </div>
       </div>
 	);
 }
 
 const TimeAdjuster = (props) => {
+    const { addTime, subtractTime } = props;
 
 	return(
       <div className="time-adjuster-container">
@@ -65,9 +100,33 @@ const TimeAdjuster = (props) => {
           {props.time}
         </h3>
         <div className="arrow-container">
-          <img className="arrow up-arrow" src={upArrow} />
-          <img className="arrow down-arrow" src={downArrow} />
+          <img
+            className="arrow up-arrow" 
+            src={upArrow} 
+            onClick= {addTime} 
+          />
+          <img 
+            className="arrow down-arrow" 
+            src={downArrow} 
+            onClick={subtractTime} 
+          />
         </div>
+      </div>
+	)
+}
+
+const TimeRemaining = (props) => {
+	const { timeRemaining } = props.state;
+	return (
+      <div className="time-remaining-container">
+      <p className="time-remaining">
+        {
+          timeRemaining === "" ?
+            ""
+          :
+            "Time left " + timeRemaining
+        }
+      </p>
       </div>
 	)
 }
@@ -98,14 +157,73 @@ class App extends Component {
 			timeIsAlmostUp: false,
 			isSession: true
 		};
-	}
+
+		this.addSessionTime = this.addSessionTime.bind(this);
+		this.addBreakTime = this.addBreakTime.bind(this);
+		this.subtractSessionTime = this.subtractSessionTime.bind(this);
+		this.subtractBreakTime = this.subtractBreakTime.bind(this);
+    }
+
+  addSessionTime(){
+  	let { sessionLength } = this.state;
+    sessionLength < 60 ?
+      this.setState({
+        sessionLength: ++sessionLength
+      })
+    :
+    alert("Session cannot be set to more than 60")
+  }
+
+  addBreakTime(){
+  	let { breakLength } = this.state;
+  	breakLength < 60 ?
+  	  this.setState({
+  	    breakLength: ++breakLength
+  	  })
+  	:
+  	alert("Break cannot be set to more than 60")
+  }
+
+  subtractSessionTime(){
+  	let { sessionLength } = this.state;
+  	sessionLength > 1 ?
+  	  this.setState({
+  	  	sessionLength: --sessionLength
+  	  })
+  	:
+  	alert("Session cannot be set to less than 1")
+  }
+
+  subtractBreakTime(){
+  	let { breakLength } = this.state;
+  	breakLength > 1 ?
+  	  this.setState({
+  	  	breakLength: --breakLength
+  	  })
+  	:
+  	alert("Break cannot be set to less than 1")
+  }
 
 	render() {
-		const { state } = this;
+		const { 
+		  state,
+		  addSessionTime,
+		  addBreakTime,
+		  subtractSessionTime,
+		  subtractBreakTime
+		} = this;
+
 		return (
 			<div className="app">
 			  <Header />
-			  <Dial state={state}/>
+			  <Dial
+			    state={state} 
+			    addSessionTime={addSessionTime}
+			    addBreakTime={addBreakTime}
+			    subtractSessionTime={subtractSessionTime}
+			    subtractBreakTime={subtractBreakTime}
+			   />
+			   <TimeRemaining state={state} />
 			  <Footer />
 			</div>
 		)
