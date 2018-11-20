7fce7b547251abb8492441fa8ef1026d2f6d08d6
JeffAbney <jeffmabney@gmail.com>
2018-11-20 13:36:35 -0600

    add font-loader

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
 	    use: ["style-loader", "css-loader"]	
 	  },
     {
-        test: /\.(png|jpg|gif)$/,
+        test: /\.(png|jpg|gif|ttf)$/,
         use: [
           {
             loader: 'file-loader',
